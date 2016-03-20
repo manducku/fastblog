c5229d4b1c824e4bfa74184cbf657ab25f6e4c20
manducku <alwaysfun2183@gmail.com>
2016-03-20 15:39:30 +0900

    post fbv implementation

--- fastblog/posts/views/posts.py
+++ fastblog/posts/views/posts.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render,redirect
 from posts.models import Post 
+from django.core.urlresolvers import reverse
 
 def posts(request):
-    
-    context = { 
-        "posts" = Post.objects.all()
-            }
 
-    return render(
-            request,
-            "posts/posts.html",
-            context = context, 
-            )
+    if(request.method =="GET"):
+        context = { 
+                "posts": Post.objects.all()
+                }
+        print ("hello")
+        return render(
+                request,
+                "posts/posts.html",
+                context = context, 
+                )
+        
+    if(request.method =="POST"):
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        post=Post.objects.create(
+                title=title,
+                content=content,
+                )
+        return redirect(
+                reverse(
+                    "posts"
+                    )
+                )
 
 
