58612b73a8cf5ed8052fa4687ff80afc085cf9d5
manducku <alwaysfun2183@gmail.com>
2016-03-20 15:07:30 +0900

    posts url add

--- fastblog/fastblog/urls.py
+++ fastblog/fastblog/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from fastblog.views import HomeView
+from posts.views import posts
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^home/', HomeView.as_view()),
+    url(r'^posts/', posts, name = "posts"),
 ]
