995dae5e0ba2f271b1b07e6e81ed32df8e96a284
manducku <alwaysfun2183@gmail.com>
2016-03-20 17:20:29 +0900

    redirect to post-detail add

--- fastblog/posts/views/post_detail.py
+++ fastblog/posts/views/post_detail.py
@@ -1,19 +1,34 @@
-from posts.models import Post 
+from posts.models import Post
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 
+
 def post_detail(request, pk):
-
-    post = Post.objects.get(pk=pk)
     
-    context = {
-            "post": post,
-            }
+    post = Post.objects.get(pk=pk)
+    if(request.method =="GET"):
+        context = {
+                "post": post,
+                }
 
-    return render(
-            request, 
-            "posts/post.html",
-            context = context,
-            )
+        return render(
+                request, 
+                "posts/post_detail.html",
+                context = context,
+                )
+    if(request.method=="POST"):
+        comment = post.comment_set.create(
+                content=request.POST.get("content")
+                )
+
+        return redirect(
+                reverse(
+                    "post-detail", 
+                    kwargs={
+                        'pk':post.id,
+                        },
+                    ), 
+                )
+
 
 
