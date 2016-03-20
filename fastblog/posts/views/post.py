f8b1ebe5860b5b0b6a9ed3d018a183ab7f10d1fb
manducku <alwaysfun2183@gmail.com>
2016-03-20 15:54:14 +0900

    detail view add

---
+++ fastblog/posts/views/post.py
@@ -0,0 +1,19 @@
+from posts.models import Post 
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+
+def post(request, pk):
+
+    post = Post.objects.get(pk=pk)
+    
+    context = {
+            "post": post,
+            }
+
+    return render(
+            request, 
+            "posts/post.html",
+            context = context,
+            )
+
+
