f34b70848fbc2a91f322eb614ba89b75c6586c86
manducku <alwaysfun2183@gmail.com>
2016-03-20 20:29:21 +0900

    add get_absolute url

--- fastblog/posts/models/post.py
+++ fastblog/posts/models/post.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Post(models.Model):
 
@@ -15,11 +16,18 @@ class Post(models.Model):
         verbose_name = "POST"
         verbose_name_plura= verbose_name
 
-
     def __str__(self,):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse(
+                'post-detail',
+                kwargs={
+                    'pk':self.id
+                    },
+                )
 
+            
 
 
 
