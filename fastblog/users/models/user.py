8407df878da27bed7470778a13afddcb67202115
manducku <alwaysfun2183@gmail.com>
2016-03-21 00:16:21 +0900

    phoneNumber -> phonenumber

--- fastblog/users/models/user.py
+++ fastblog/users/models/user.py
@@ -2,14 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-
-    phoneNumber = models.CharField(
+    phonenumber = models.CharField(
             max_length=20,
             blank=True,
             null=True,
             )
 
-    class META:
+    class Meta:
         verbose_name = "member info"
         verbose_name_plural = verbose_name
 
