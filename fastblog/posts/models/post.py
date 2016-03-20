02bb9a938e8862030d779fd8b156e433625565bc
manducku <alwaysfun2183@gmail.com>
2016-03-20 14:39:45 +0900

    cotent --> content

--- fastblog/posts/models/post.py
+++ fastblog/posts/models/post.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
 
     title = models.CharField(max_length = 30)
-    cotent = models.TextField()
+    content = models.TextField()
     create_at = models.DateTimeField(
             auto_now_add = True, 
             )
